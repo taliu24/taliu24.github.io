7660928da102364fcfd622c22d18ba0f874f6cb8
taliu24 <taliu24@gmail.com>
2016-07-29 10:38:30 -0700

    initial upload
    Farrell Eyecare Website

---
+++ js/project.js
@@ -0,0 +1,13 @@
+// $.fn.tooltip.Constructor.VERSION// => "3.3.7"
+
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// })
+
+
+// $('[data-toggle="popover"]').popover({trigger: 'hover','placement': 'top'});
+
+$(function() {
+
+  $('#popoverOption').popover({ trigger: "hover" });
+});
